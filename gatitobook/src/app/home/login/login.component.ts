4e359aa7dd1279d8cba3c64267134531f4242f2a
Miguel Fialho <fialhomig@gmail.com>
2022-06-10 16:52:21 -0300

    alterando para tela de animais ao autenticar

--- gatitobook/src/app/home/login/login.component.ts
+++ gatitobook/src/app/home/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AutenticacaoService } from 'src/app/autenticacao/autenticacao.service';
 
 @Component({
@@ -9,14 +10,17 @@ import { AutenticacaoService } from 'src/app/autenticacao/autenticacao.service';
 export class LoginComponent implements OnInit {
   usuario = '';
   senha = '';
-  constructor(private authService: AutenticacaoService) {}
+  constructor(
+    private authService: AutenticacaoService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {}
 
   login() {
     this.authService.autenticar(this.usuario, this.senha).subscribe(
       () => {
-        console.log('Atenticacao com sucesso');
+        this.router.navigate(['animais']);
       },
       (error) => {
         alert('Usuario ou senha inválido');
