4dceb4fb7f0fdb92f30fb0ebea38c9f1f5de528d
Miguel Fialho <fialhomig@gmail.com>
2022-06-10 16:36:28 -0300

    implementando login

--- gatitobook/src/app/home/login/login.component.ts
+++ gatitobook/src/app/home/login/login.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { AutenticacaoService } from 'src/app/autenticacao/autenticacao.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  usuario = '';
+  senha = '';
+  constructor(private authService: AutenticacaoService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  login() {
+    this.authService.autenticar(this.usuario, this.senha).subscribe(
+      () => {
+        console.log('Atenticacao com sucesso');
+      },
+      (error) => {
+        alert('Usuario ou senha inválido');
+        console.log(error);
+      }
+    );
   }
-
 }
