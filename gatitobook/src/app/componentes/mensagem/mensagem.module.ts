f54811a64220fda1111ea88df4f8c6c94f24ba64
Miguel Fialho <fialhomig@gmail.com>
2022-06-11 09:39:33 -0300

    Adicionando o componente de mensagem e usando-o para validações de login

--- gatitobook/src/app/componentes/mensagem/mensagem.module.ts
+++ gatitobook/src/app/componentes/mensagem/mensagem.module.ts
@@ -2,12 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MensagemComponent } from './mensagem.component';
 
-
-
 @NgModule({
   declarations: [MensagemComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  exports: [MensagemComponent],
 })
-export class MensagemModule { }
+export class MensagemModule {}
