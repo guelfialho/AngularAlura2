5e2a9d1b89ded4339accba52eaf376814e64bde4
Miguel Fialho <fialhomig@gmail.com>
2022-06-18 12:11:44 -0300

    Usando guarda resolver para agilidade

---
+++ gatitobook/src/app/animais/detalhe-animal/comentarios/comentarios.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Comentarios } from './comentarios';
+const API = environment.apiURL;
+@Injectable({
+  providedIn: 'root',
+})
+export class ComentariosService {
+  constructor(private http: HttpClient) {}
+
+  buscaComentario(id: number): Observable<Comentarios> {
+    return this.http.get<Comentarios>(`${API}/photos/${id}/comments`);
+  }
+  incluiComentario(id: number, commentText: string): Observable<Comentarios> {
+    return this.http.post<Comentarios>(`${API}/photos/${id}/comments`, {
+      commentText,
+    });
+  }
+}
